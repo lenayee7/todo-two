28c1977d148cfa2f4939500e58734f0486833fe1
Fishie-dev <fishie-dev@Lena-MacBook-Pro.local>
2018-08-30 20:24:37 -0700

    Add edit/update TodoList functionality

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -13,14 +13,25 @@ class TodoListsController < ApplicationController
 		@todo_list = TodoList.new
 	end
 
+	def edit
+		@todo_list = TodoList.find(params[:id])
+		render 'edit'
+	end
+
 	def create
 		todo_list_data = params[:todo_list]
 		title = todo_list_data[:title]
 		description = todo_list_data[:description]
-		@todo_list = TodoList.create(title: title, description: description)
+		@todo_list = TodoList.create({title: title, description: description})
 		@todo_list_items = @todo_list.todo_list_items.all
-		
 		render 'show'
+	end
+
+	def update
+		@todo_list = TodoList.find(params[:id])
+		@todo_list.update(todo_list_params)
+
+		redirect_to todo_list_path, notice: "OMG \"#{@todo_list.title}\" has been updated."
 	end
 
 	def destroy
@@ -29,6 +40,12 @@ class TodoListsController < ApplicationController
 		@todo_list.destroy
 		flash[:notice] = "DELETED TODO LIST"
 		render 'index'
+	end
+
+private
+
+	def todo_list_params
+ 		params.require(:todo_list).permit(:title, :description)
 	end
 
 end
