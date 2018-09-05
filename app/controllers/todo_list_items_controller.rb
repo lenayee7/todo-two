12a1ee44f2bbddd999f9aea55b5e3c3fa587e58a
Fishie-dev <fishie-dev@Lena-MacBook-Pro.local>
2018-09-04 20:02:41 -0700

    Allows you to update TodoList list-item

--- app/controllers/todo_list_items_controller.rb
+++ app/controllers/todo_list_items_controller.rb
@@ -5,17 +5,38 @@ class TodoListItemsController < ApplicationController
 		@todo_list_item = TodoListItem.new(todo_list_id: params[:todo_list_id])
 	end
 
-	def create
-		todo_list_item_data = params[:todo_list_item]
-		name = todo_list_item_data[:name]
-		description = todo_list_item_data[:description]
-		priority = todo_list_item_data[:priority]
-		completed = todo_list_item_data[:completed]
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_list_item = @todo_list.todo_list_items.find(params[:id])
 
-		@todo_list_item = TodoListItem.create!(todo_list_id: params[:todo_list_id], name: name, description: description, priority: priority, completed: completed)
+    render 'edit'
+  end
 
-		flash[:notice] = "YaYYYY you created a List Item"
-		redirect_to "/todo_lists/#{params[:todo_list_id]}"
-	end
+  def create
+    todo_list_item_data = params[:todo_list_item]
+    name = todo_list_item_data[:name]
+    description = todo_list_item_data[:description]
+    priority = todo_list_item_data[:priority]
+    completed = todo_list_item_data[:completed]
+
+    @todo_list_item = TodoListItem.create!(todo_list_id: params[:todo_list_id], name: name, description: description, priority: priority, completed: completed)
+
+    flash[:notice] = "YaYYYY you created a List Item"
+    redirect_to "/todo_lists/#{params[:todo_list_id]}"
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_list_item = @todo_list.todo_list_items.find(params[:id])
+    @todo_list_item.update(todo_list_item_params)
+
+    redirect_to "/todo_lists/#{params[:todo_list_id]}", notice: "ARF Name: \"#{@todo_list_item.name}\" has been updated."
+  end
+
+  private
+
+  def todo_list_item_params
+    params.require(:todo_list_item).permit(:name, :description, :priority, :completed)
+  end
 
 end
