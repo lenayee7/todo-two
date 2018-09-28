6c71636b828b8d03cd73d9926f65c3b725431597
Edmundo Martinez <edmundonm@gmail.com>
2018-09-27 21:03:21 -0700

    Inject HTML content for created todo list item

--- app/assets/javascripts/todo_lists/todo_list_item_form.js
+++ app/assets/javascripts/todo_lists/todo_list_item_form.js
@@ -10,17 +10,37 @@ $(document).ready(function() {
         completed: false
       }
     };
-
+    debugger
+    var todoListId = $('#todo-list-id-hidden-field').val();
+    var url = 'http://localhost:3000/todo_lists/' + todoListId + '/todo_list_items.json';
     event.preventDefault();
     event.stopPropagation();
 
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:3000/todo_lists/8/todo_list_items.json',
+      url: url,
       data: data,
       success: function(data) {
         alert('You did it');
-        location.reload();
+        debugger
+        // Inject HTML into our page
+        // Find the element we're adding things into
+        var items = $('#todo-list-items');
+        // generate html
+        var html = "<div class='col s12 m6'><div class='card'><div class='card-content'>\
+                      <div>Name: NAME</div>\
+                      <div>Description: DESCRIPTION</div>\
+                      <div>Priority: PRIORITY</div>\
+                      <div>Incomplete</div>\
+                      <div class='card-action'>TODO: insert links here</div>\
+                    </div></div></div>"
+        // Replace placeholders with actual DATA https://www.w3schools.com/jsref/jsref_replace.asp
+        // inject: https://api.jquery.com/append/
+        items.append(html);
+        // Clear the Form
+        $("#todo-list-item-name").val('')
+        $("#todo-list-item-description").val('')
+        $("#todo-list-item-priority").val('')
       },
       error: function(jqXHR,textStatus,errorThrown) {
         alert('ERROR didnt save item');
